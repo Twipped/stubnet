e8e09726ef253e45c23a83cfc999977d2dafc424
ChiperSoft <chiper@chipersoft.com>
2015-08-21 18:38:32 -0700

    Switch from mocha to tap.

--- tests/stubnet.js
+++ tests/stubnet.js
@@ -1,94 +1,76 @@
 
+var test    = require('tap').test;
+var sinon   = require('sinon');
 var stubnet = require('../index');
-var sinon = require('sinon');
-var assert = require('assert');
-var net = require('net');
+var net     = require('net');
+var AssertionError = require('assert').AssertionError;
 
 
-exports['basic connection'] = function (done) {
-	var stubs = {
-		ok: sinon.stub(),
-		equal: sinon.stub()
-	};
+var inspect = require('util').inspect;
 
-	var stub = stubnet({assert:stubs})
-		.listenTo({port: 50000, ready: onReady, message: 'LISTEN'})
-		.expectConnection('CONNECTION OPENED')
-		.expectData('hello', 'DATA')
-		.expectDisconnect('CONNECTION ENDED')
-		.start(onFinish);
+function failureHook (t) {
+	return sinon.spy(function () {
+		t.pass(this.name);
+	});
+}
+
+test('basic connection', function (t) {
+
+	var mockServer = stubnet()
+		.listenTo({port: 50000, pass: onReady, fail: t.fail, name: 'listenTo'})
+		.expectConnection(     {pass: t.pass,  fail: t.fail, name: 'expectConnection'})
+		.expectData('hello',   {pass: t.pass,  fail: t.fail, name: 'expectData'})
+		.expectDisconnect(     {pass: t.pass,  fail: t.fail, name: 'expectDisconnect'})
+		.start({                pass: t.pass,  done: t.end,  name: 'server is finished'});
+
+	t.ok(mockServer.abort, 'abort function exists');
 
 	function onReady() {
 		var socket = new net.Socket();
 		socket.connect(50000, 'localhost', function () {
+			t.pass('net.connect');
 			socket.write('hello', function () {
-				assert.ok(true, 'Sent hello');
+				t.pass('net.send "hello"');
 				socket.end(function () {
-					assert.ok(true, 'Closed connection');
+					t.pass('net.end');
+				});
+			})
+		})
+	}
+
+});
+
+
+test('unexpected connection and wrong data', function (t) {
+
+	var stub = failureHook(t);
+
+	var mockServer = stubnet()
+		.listenTo({port: 50000, pass: onReady, fail: stub, name: 'listenTo'})
+		.expectData('testing', {pass: t.fail,  fail: stub, name: 'expectData'})
+		.start({pass: t.pass,  done: onFinish,  name: 'server is finished'});
+
+	function onReady() {
+		var socket = new net.Socket();
+		socket.connect(50000, 'localhost', function () {
+			t.pass('net.connect')
+			socket.write('hello', function () {
+				t.pass('net.send "hello"');
+				socket.end(function () {
+					t.pass('net.end');
 				});
 			})
 		})
 	}
 
 	function onFinish() {
-		sinon.assert.callCount(stubs.ok, 4);
-		sinon.assert.callCount(stubs.equal, 0);
+		t.equal(stub.callCount, 1, 'only one failure detected');
 
-		sinon.assert.calledWithExactly(stubs.ok, true, 'LISTEN');
-		sinon.assert.calledWithExactly(stubs.ok, true, "CONNECTION OPENED");
-		sinon.assert.calledWithExactly(stubs.ok, true, 'DATA');
-		sinon.assert.calledWithExactly(stubs.ok, true, 'CONNECTION ENDED');
+		var failureValue = stub.getCall(0).args[0];
+		t.ok(failureValue instanceof AssertionError, 'received an AssertionError');
+		t.equal(failureValue.message, 'Unexpected connection received');
 
-		sinon.assert.callOrder(
-			stubs.ok.withArgs(true, 'LISTEN'),
-			stubs.ok.withArgs(true, "CONNECTION OPENED"),
-			stubs.ok.withArgs(true, 'DATA'),
-			stubs.ok.withArgs(true, 'CONNECTION ENDED')
-		);
-
-		done();
-	}
-};
-
-
-exports['unexpected connection and wrong data'] = function (done) {
-	var stubs = {
-		ok: sinon.stub(),
-		equal: sinon.stub()
-	};
-
-	var stub = stubnet({assert:stubs})
-		.listenTo({port: 50000, ready: onReady, message: 'LISTEN'})
-		.expectData('testing', 'EXPECTDATA')
-		.start(onFinish);
-
-	function onReady() {
-		var socket = new net.Socket();
-		socket.connect(50000, 'localhost', function () {
-			assert.ok(true, 'connected')
-			socket.write('hello', function () {
-				assert.ok(true, 'Sent hello');
-				socket.end(function () {
-					assert.ok(true, 'Closed connection');
-				});
-			})
-		})
+		t.end();
 	}
 
-	function onFinish() {
-		sinon.assert.callCount(stubs.ok, 2);
-		sinon.assert.callCount(stubs.equal, 1);
-
-		sinon.assert.calledWithExactly(stubs.ok, true,  'LISTEN');
-		sinon.assert.calledWithExactly(stubs.ok, false, "Unexpected connection received (EXPECTDATA)");
-		sinon.assert.calledWithExactly(stubs.equal, 'hello', 'testi', 'EXPECTDATA');
-
-		sinon.assert.callOrder(
-			stubs.ok.withArgs(true,  'LISTEN'),
-			stubs.ok.withArgs(false, "Unexpected connection received (EXPECTDATA)"),
-			stubs.equal.withArgs('hello', 'testi', 'EXPECTDATA')
-		);
-
-		done();
-	}
-};
+});
