d01f3f846b782f72d565fd92a1ef11a2cb2c4dbc
ChiperSoft <chiper@chipersoft.com>
2015-08-22 10:51:22 -0700

    Add test for closing from server side, fix closing from server side.

---
+++ tests/0004.js
@@ -0,0 +1,39 @@
+
+var test    = require('tap').test;
+var stubnet = require('../index');
+var net     = require('net');
+var Promise = require('bluebird');
+var proxmis = require('proxmis');
+
+test('expected connection and valid data, with close', function (t) {
+	t.plan(8);
+
+	var stubnetDone = proxmis({noError: true});
+	var socketDone  = proxmis({noError: true});
+
+	var mockServer = stubnet()
+		.listenTo({port: 50000, pass: onReady, fail: t.fail, name: 'listenTo'})
+		.expectConnection(     {pass: t.pass,  fail: t.fail, name: 'expectConnection'})
+		.expectData('hello',   {pass: t.pass,  fail: t.fail, name: 'expectData'})
+		.thenClose(            {pass: t.pass,  fail: t.fail, name: 'thenClose'})
+		.start({                pass: t.pass,  fail: t.fail, done: stubnetDone,  name: 'server is finished'});
+
+	t.ok(mockServer.abort, 'abort function exists');
+
+	function onReady() {
+		var socket = new net.Socket();
+		socket.connect(50000, 'localhost', function () {
+			t.pass('net.connect');
+			socket.write('hello', function () {
+				t.pass('net.send "hello"');
+			});
+			socket.on('end', function () {
+				t.pass('net.ended');
+				socketDone();
+			});
+		});
+	}
+
+	Promise.join(stubnetDone, socketDone).then(t.end);
+
+});
